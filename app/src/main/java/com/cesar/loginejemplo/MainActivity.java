9008a57480527e027ec390ec12d57c266f6d8e98
Cesar Zapien <cesaremiliozapien5@gmail.com>
2024-05-15 12:40:49 -0600

    Creacion del repositorio

---
+++ app/src/main/java/com/cesar/loginejemplo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.cesar.loginejemplo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
